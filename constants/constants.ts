20d8c113e9d5ac5f91993da6d279a94f5f6e8587
Abdulshakur Ahmed <hanadderia@gmail.com>
2025-05-11 12:04:54 +0300

    Update: Introduced Read Me docs

--- constants/constants.ts
+++ constants/constants.ts
@@ -1,5 +1,1 @@
 export const baseURL = process.env.NEXT_PUBLIC_API_URL;
-const apiKey = process.env.NEXT_PUBLIC_API_Key;
-const apiSecret = process.env.NEXT_PUBLIC_API_Secret;
-
-export const apiAuth = `${apiKey}:${apiSecret}`;
