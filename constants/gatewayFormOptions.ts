f900bd5025e2f95d46ba776c1e1637505ed6d290
Abdulshakur Ahmed <hanadderia@gmail.com>
2025-07-12 15:48:03 +0300

    feat: added amana bank to the bic options

--- constants/gatewayFormOptions.ts
+++ constants/gatewayFormOptions.ts
@@ -1,8 +1,8 @@
-export const verificationMethods  = [
-    { value: 'ACCT', label: 'Account' },
-    { value: 'EWLT', label: 'Wallet' },
-    { value: 'MSIS', label: 'Phone Number' },
-    { value: 'IBAN', label: 'IBAN (International Bank Account Number)' },
+export const verificationMethods = [
+  { value: "ACCT", label: "Account" },
+  { value: "EWLT", label: "Wallet" },
+  { value: "MSIS", label: "Phone Number" },
+  { value: "IBAN", label: "IBAN" },
 ];
 
 export const bicOptions = [
@@ -19,4 +19,5 @@ export const bicOptions = [
   { value: "ZKBASOS0", label: "Ziraat Katilim Bank Somalia" },
   { value: "IBOSSOS0", label: "International Bank Of Somalia" },
   { value: "SOMNSOS0", label: "SOMBANK LTD" },
+  { value: "AMBKSOS0", label: "Amana Bank" },
 ];
