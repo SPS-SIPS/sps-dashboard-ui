3ccd81eebaa0ad81b49e55bab3c5f804488f7400
Abdihakim Mohamed <abdihakin18@gmail.com>
2026-01-06 13:29:48 +0300

    Add 'Developed by SPS Team' line to footer

--- component/Layout/Footer/Footer.tsx
+++ component/Layout/Footer/Footer.tsx
@@ -7,6 +7,7 @@ const Footer = () => {
     <footer className={styles.footer}>
       <div className={styles.footerBottom}>
         <p>© {currentYear} Somali Payment Switch. All rights reserved.</p>
+          <p className={styles.developedBy}>Developed by SPS Team</p>
       </div>
     </footer>
   );
