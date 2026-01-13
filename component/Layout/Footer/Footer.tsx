3f3fca7ad104a9721a757a02ee1f21f23ec1056b
Abdihakim Mohamed <abdihakin18@gmail.com>
2026-01-13 11:21:37 +0300

    feat: add heartbeat heart icon and improve footer text color for better contrast

--- component/Layout/Footer/Footer.tsx
+++ component/Layout/Footer/Footer.tsx
@@ -1,16 +1,19 @@
+import { FaHeart } from "react-icons/fa";
 import styles from "./Footer.module.css";
 
 const Footer = () => {
-  const currentYear = new Date().getFullYear();
+    const currentYear = new Date().getFullYear();
 
-  return (
-    <footer className={styles.footer}>
-      <div className={styles.footerBottom}>
-        <p>© {currentYear} Somali Payment Switch. All rights reserved.</p>
-          <p className={styles.developedBy}>Developed by SPS Team</p>
-      </div>
-    </footer>
-  );
+    return (
+        <footer className={styles.footer}>
+            <div className={styles.footerBottom}>
+                <p>© {currentYear} Somali Payment Switch. All rights reserved.</p>
+                <p className={styles.developedBy}>
+                    Developed with <FaHeart className={styles.heartIcon} /> by SPS Team
+                </p>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
